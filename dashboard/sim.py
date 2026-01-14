69bf7260e36f1d813fc8078ef4f776d68c7a4515
Nikhil Mani <nikhilrmani@gmail.com>
2026-01-14 13:20:29 -0500

    refactor simulation into class and add random switching state logic

--- dashboard/sim.py
+++ dashboard/sim.py
@@ -2,23 +2,13 @@ import random as rand
 from datetime import datetime
 import http.client
 import json
-
+import time 
 # by parsing data, I found that the range between the highest and lowest values was 2
 # this code works based off that assumption 
 
 closed_vals = [12, 30] # these are the values it should roughly take on when closed, depending on position
+open_vals = [63, 122]
 r = 2
-def gen_sonar(val, spread):
-    return rand.uniform(val-(spread/2), val+(spread/2))
-
-def gen_time():
-    t = datetime.now().time()
-    return (t.hour * 3600 + t.minute * 60 + t.second) * 1000000 + t.microsecond
-def gen_post(val, r):
-    data = []
-    for i in range(5):
-        data.append({"sonar_distance": f"{gen_sonar(val, r)}", "time" : f"{gen_time()}"})
-    return data
 class HTTP:
     def __init__(self, url: str):
         self.url = url
@@ -30,6 +20,44 @@ class HTTP:
         response = self.conn.getresponse()
         print(response.status, response.reason)
 
-x = HTTP("10.203.116.51:5001")
-data = gen_post(closed_vals[0], r)
-x.post_data(data)
+class Simulation:
+
+    def __init__(self, spread, url):
+        self.spread = spread
+        self.state = False # True is open, closed is False
+        self.url = url
+        self.time_until_switch = rand.randint(3, 8)
+        self.vals = closed_vals
+        self.http = HTTP(self.url)
+    def gen_sonar(self,val):
+        return rand.uniform(val-(self.spread/2), val+(self.spread/2))
+    
+    def gen_time(self):
+        t = datetime.now().time()
+        return (t.hour * 3600 + t.minute * 60 + t.second) * 1000000 + t.microsecond
+    
+    def flip_state(self):
+        self.state = not self.state
+        if(self.state):
+            self.vals = open_vals
+        else: 
+            self.vals = closed_vals
+        self.time_until_switch = rand.randint(3, 8)
+
+    def gen_post(self):
+        data = []
+        for i in range(5):
+            data.append({"sonar_distance": f"{self.gen_sonar(rand.choice(self.vals[0]), self.spread)}", "time" : f"{self.gen_time()}"})
+        self.http.post_data(data)
+
+    def step(self):
+        self.time_until_switch-=1
+
+        if(self.time_until_switch == 0):
+            self.flip_state()
+            self.gen_post()
+
+sim = Simulation(r, "10.203.116.51:5001")
+for i in range(100):
+    sim.step()
+    time.sleep(.001)
